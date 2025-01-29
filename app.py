445dc4346bf0340d4bcf4f08eca0b37069e18b51
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2025-01-29 13:36:40 +0700

    add app

---
+++ app.py
@@ -0,0 +1,45 @@
+import gradio as gr
+from agents import agent, createConnection, getAllTable
+
+def connect(username, password, host, database):
+    print("test")
+    global conn
+    conn = createConnection(username, password, host, database)
+    if conn:
+        tables = getAllTable(conn)  # Mengambil data setelah koneksi berhasil
+        return (
+            conn, 
+            gr.update(value="✅ Connect Success", visible=True),  # Menampilkan toast sukses
+            gr.update(value=tables, visible=True)  # Menampilkan tabel setelah connect
+        )
+    return None, gr.update(value="❌ Connection Failed", visible=True), gr.update(visible=False)
+
+def yapping(message,history):
+    print(history)
+    global conn
+    result = agent.run(message)
+    return str(result) 
+
+
+with gr.Blocks() as demo:
+    state = gr.State(None)
+    gr.Markdown("# Yapping to SQL")
+
+    with gr.Tab("Chatbot"):
+        gr.ChatInterface(fn=yapping, type='messages',title="Chatsql")
+
+    with gr.Tab("Database"):
+        host = gr.Textbox(placeholder="http://127.0.0.1/", label="Host")
+        username = gr.Textbox(placeholder="Username", label="Username")
+        password = gr.Textbox(placeholder="Password", type="password", label="Password")
+        database = gr.Textbox(placeholder="Database", label="Database")
+
+        toast = gr.Markdown(visible=False)  # Feedback setelah tombol ditekan
+        btn = gr.Button("Connect")
+
+        gr.Markdown("### Database Info")
+        tables_output = gr.Textbox(visible=False)  # Awalnya disembunyikan
+
+        btn.click(fn=connect, inputs=[username, password, host, database], outputs=[state, toast, tables_output])
+
+demo.launch()
